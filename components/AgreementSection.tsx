0bc6a4dc3852355ccd52648202abc20ba10a9b86
Buari Olatunji <buariolatunji@gmail.com>
2026-02-16 00:06:44 +0100

    Name update

--- components/AgreementSection.tsx
+++ components/AgreementSection.tsx
@@ -253,8 +253,7 @@ export const AgreementSection: React.FC = () => {
                                 d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                               />
                             </svg>
-                            Schedule Truce (Duration){" "}
-                            <span className="text-amber-500">*</span>
+                            Duration <span className="text-amber-500">*</span>
                           </label>
                           <div className="grid grid-cols-2 gap-5">
                             <input
